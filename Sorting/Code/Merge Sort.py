5c2c0a37cf954fd2d11b37a8eef46dccb6c2fbc1
Yashdeep Singh <107052354+Yash5204@users.noreply.github.com>
2022-06-19 00:20:57 +0530

    Sudoku solver code, Merge Sort notes and homework problem solution in python completed

---
+++ Sorting/Code/Merge Sort.py
@@ -0,0 +1,48 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return
+
+    mid = len(arr)//2
+
+    left = arr[:mid]
+    right = arr[mid:]
+
+    merge_sort(left)
+    merge_sort(right)
+
+    merge_two_sorted_lists(left, right, arr)
+
+def merge_two_sorted_lists(a,b,arr):
+    len_a = len(a)
+    len_b = len(b)
+
+    i = j = k = 0
+
+    while i < len_a and j < len_b:
+        if a[i] <= b[j]:
+            arr[k] = a[i]
+            i+=1
+        else:
+            arr[k] = b[j]
+            j+=1
+        k+=1
+
+    while i < len_a:
+        arr[k] = a[i]
+        i+=1
+        k+=1
+
+    while j < len_b:
+        arr[k] = b[j]
+        j+=1
+        k+=1
+
+if __name__ == '__main__':
+    nums = [
+        [5,2,3,1],
+        [5,1,1,2,0,0]
+    ]
+
+    for arr in nums:
+        merge_sort(arr)
+        print(arr)
