f4f7270e743014a165cba665facf8af6891ddd09
Yashdeep Singh <107052354+Yash5204@users.noreply.github.com>
2022-06-19 19:22:51 +0530

    Create Merge Sort.cpp

---
+++ Sorting/Homework/Merge Sort.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    vector<int> merge(vector<int>& A,vector<int>& B){
+        int sizeA = A.size();
+        int sizeB = B.size();
+        int i = 0;
+        int j = 0;
+        vector<int> ans;
+
+        // Compare and fill ans vector
+       while(i<sizeA && j<sizeB){
+            if(A[i] < B[j]){
+                ans.push_back(A[i]);
+                i = i+1;
+            } else{
+                ans.push_back(B[j]);
+                j = j+1;
+            }
+        }
+        // Add remaining elements fromA
+        while(i<sizeA){
+            ans.push_back(A[i]);
+            i = i+1;
+        }
+        // Add remaining elements fromB
+        while(j<sizeB){
+            ans.push_back(B[j]);
+            j = j+1;
+        }
+        return ans;
+    }
+
+    vector<int> mergeSort(vector<int>& nums,int start,int end){
+        // Base case
+        if(start > end){
+            return {};
+        }
+        if(start == end){
+            return {nums[start]};
+        }
+        // Calculate Mid
+        int mid = start + (end-start)/2;
+
+        // MergeSortCall
+        vector<int> A = mergeSort(nums,start,mid);
+        vector<int> B = mergeSort(nums,mid+1,end);
+        
+        // MergeCall
+        return merge(A,B);
+    }
+        vector<int> sortArray(vector<int>& nums) {
+        int n = nums.size();
+        return mergeSort(nums,0,n-1);
+    }
+};
