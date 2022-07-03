b966c267bc782b67e0d9a84fc529d8b00348094b
Shivansh Singh <shivanshsinghx365@gmail.com>
2022-07-03 21:37:31 +0530

    Arrays Note, Code & HW Added

---
+++ C++/Code/array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //declaration
+    int a[4]={1,2,3,4};
+
+    //printing
+    for(int i=0;i<4;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+
+    //accessing
+    cout<<a[3]<<endl;
+
+
+}
